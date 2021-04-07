4b3599f8dfc3bbd9935c06659f7993a394a4acec
Lai Alexander <laialexanderholmsen@gmail.com>
2021-04-07 23:38:33 +0200

    Add first working skeleton

--- Notes/Form1.Designer.cs
+++ Notes/Form1.Designer.cs
@@ -29,13 +29,141 @@ namespace Notes
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.bodyLabel = new System.Windows.Forms.Label();
+            this.TitleTextBox = new System.Windows.Forms.TextBox();
+            this.BodyTextBox = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.NewButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Location = new System.Drawing.Point(129, 51);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(38, 20);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Title";
+            // 
+            // bodyLabel
+            // 
+            this.bodyLabel.AutoSize = true;
+            this.bodyLabel.Location = new System.Drawing.Point(124, 125);
+            this.bodyLabel.Name = "bodyLabel";
+            this.bodyLabel.Size = new System.Drawing.Size(43, 20);
+            this.bodyLabel.TabIndex = 1;
+            this.bodyLabel.Text = "Body";
+            // 
+            // TitleTextBox
+            // 
+            this.TitleTextBox.Location = new System.Drawing.Point(173, 51);
+            this.TitleTextBox.Name = "TitleTextBox";
+            this.TitleTextBox.Size = new System.Drawing.Size(125, 27);
+            this.TitleTextBox.TabIndex = 2;
+            // 
+            // BodyTextBox
+            // 
+            this.BodyTextBox.Location = new System.Drawing.Point(173, 125);
+            this.BodyTextBox.Multiline = true;
+            this.BodyTextBox.Name = "BodyTextBox";
+            this.BodyTextBox.Size = new System.Drawing.Size(125, 184);
+            this.BodyTextBox.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(389, 54);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(178, 255);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // NewButton
+            // 
+            this.NewButton.Location = new System.Drawing.Point(173, 355);
+            this.NewButton.Name = "NewButton";
+            this.NewButton.Size = new System.Drawing.Size(94, 29);
+            this.NewButton.TabIndex = 5;
+            this.NewButton.Text = "New";
+            this.NewButton.UseVisualStyleBackColor = true;
+            this.NewButton.Click += new System.EventHandler(this.NewButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(273, 355);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(94, 29);
+            this.SaveButton.TabIndex = 6;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(373, 355);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(94, 29);
+            this.LoadButton.TabIndex = 7;
+            this.LoadButton.Text = "Load";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(473, 355);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(94, 29);
+            this.DeleteButton.TabIndex = 8;
+            this.DeleteButton.Text = "Delete";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.LoadButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.NewButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.BodyTextBox);
+            this.Controls.Add(this.TitleTextBox);
+            this.Controls.Add(this.bodyLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label bodyLabel;
+        private System.Windows.Forms.TextBox TitleTextBox;
+        private System.Windows.Forms.TextBox BodyTextBox;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button NewButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.Button DeleteButton;
     }
 }
 
--- Notes/Form1.cs
+++ Notes/Form1.cs
@@ -12,10 +12,63 @@ namespace Notes
 {
     public partial class Form1 : Form
     {
+        DataTable notesTable;
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            notesTable = new DataTable();
+            notesTable.Columns.Add("Title", typeof(String));
+            notesTable.Columns.Add("Body", typeof(String));
+
+            dataGridView1.DataSource = notesTable;
+            dataGridView1.Columns["Body"].Visible = false;
+            dataGridView1.Columns["Title"].Width = 175;
+        }
+
+        private void NewButton_Click(object sender, EventArgs e)
+        {
+            TitleTextBox.Clear();
+            BodyTextBox.Clear();
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            notesTable.Rows.Add(TitleTextBox.Text, BodyTextBox.Text);
+
+            TitleTextBox.Clear();
+            BodyTextBox.Clear();
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentCell == null)
+            {
+                return;
+            }
+            
+            int index = dataGridView1.CurrentCell.RowIndex;
+
+            if (index >= 0)
+            {
+                TitleTextBox.Text = notesTable.Rows[index]["Title"].ToString();
+                BodyTextBox.Text = notesTable.Rows[index]["Body"].ToString();
+            }
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentCell == null)
+            {
+                return;
+            }
+
+            int index = dataGridView1.CurrentCell.RowIndex;
+
+            notesTable.Rows[index].Delete();
+        }
     }
 }
